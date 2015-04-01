7db36d72e800eadbff7a601d1660a48127dc77a4
yoshiemuranaka <yoshiemuranaka@gmail.com>
2015-04-01 14:17:23 -0400

    using data

---
+++ script.js
@@ -0,0 +1,87 @@
+$(document).ready(function(){
+
+	var counter = 0
+	var deck = [
+		{id: 0, front: "q1", back: "a1"}, 
+		{id: 1, front: "q2", back: "a2"}, 
+		{id: 2, front: "q3", back: "a3"}
+	]
+
+	var card = {
+		config: {
+			id: 0,
+			front: document.getElementsByClassName("front")[0],
+			back: document.getElementsByClassName("back")[0],
+			moveLength: 0,
+			startPoint: 0,
+			endPoint: 0,
+			container: document.getElementsByClassName("container")[0]
+		},
+
+		init: function(data){
+			card.config.id = data.id
+			card.config.front.innerHTML = data.front
+			card.config.back.innerHTML = data.back
+			card.config.container.addEventListener("touchstart", card.handleStart, false);
+			card.config.container.addEventListener("touchend", card.handleEnd, false);
+			card.config.container.addEventListener("touchmove", card.handleMove, false);
+		},
+
+		//RECORD TOUCHSTART CONFIG
+		handleStart: function(event){
+			card.config.startPoint = event.touches[0].clientX
+		},
+
+		//RECORD TOUCHMOVE CONFIG
+		handleMove: function(event){
+			card.config.moveLength += 1
+		},
+
+		//ON TOUCHEND, CHECKMOVELENGTH() AND RESET CONFIG TOUCH START, TOUCH END, MOVELENGTH
+		handleEnd: function(event){
+			card.config.endPoint = event.changedTouches[0].clientX
+			card.checkMoveLength(card.config.moveLength, card.config.startPoint, card.config.endPoint);
+			card.config.moveLength = 0;
+			card.config.endPoint = 0;
+			card.config.startPoint = 0; 
+		},
+		
+		//CHECKING THE MOVE LENGTH LOGIC TO FLIP OR SWIPE CARD
+		checkMoveLength: function(moveLength, startPoint, endPoint){
+			if(moveLength > 2 && (startPoint - endPoint) > 0){
+				card.swipeLeft()
+			}else if(moveLength > 2 && (startPoint - endPoint) < 0){
+				card.swipeRight()
+			}else{
+				$('.container').toggleClass('active')
+			}
+		}, 
+
+		//COUNTER INCREASES IF IT IS ON LAST CARD IN DECK, COUNTER RESETS TO 0/FIRST CARD IN DECK
+		swipeLeft: function(){
+			if(card.config.id == deck.length -1){
+				counter = 0,
+				card.init(deck[counter])
+			}else{
+				counter = card.config.id + 1
+				card.init(deck[counter])
+			}
+		},
+
+		//COUNTER DECREASES, IF IT IS THE FIRST CARD IN DECK, COUNTER RESETS TO ID OF LAST CARD IN DECK
+		swipeRight: function(){
+			if(card.config.id == 0){
+				counter = deck.length -1,
+				card.init(deck[counter])
+			}else{
+				counter = card.config.id - 1
+				card.init(deck[counter])
+			}
+		}
+	}
+	
+	card.init(deck[counter])
+
+})
+
+
